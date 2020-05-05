36825bdda24f242ac418d394cf344a6f0c0f4b5e
Kate Makarova <makarchiknet@gmail.com>
2020-05-05 16:52:20 +0300

    add homework_1

---
+++ script.js
@@ -0,0 +1,19 @@
+let money = +prompt("Ваш бюджет на месяц?", ""),
+   time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+const appData = {
+   budget: money,
+   timeData: time,
+   expenses: {},
+   optionalExpenses: {},
+   income: [],
+   savings: false
+}
+
+let spending = prompt("Введите обязательную статью расходов в этом месяце", ""),
+   amount = +prompt("Во сколько обойдется?", "");
+
+
+appData.expenses[spending] = amount;
+
+console.log(appData.budget / 30);
